f1f92a85d5bd4bdd027e9605e6a950d7293543ba
Brad <brad@viziworx.com>
2019-11-01 11:50:38 -0400

    Added more styling to player list

--- client/src/Components/PlayersList.js
+++ client/src/Components/PlayersList.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Player from './Player';
 
 function PlayersList(props) {
 	console.log('Players List:', props);
-	return <div className='Players-list'>Players list</div>;
+	return (
+		<div className='Players-list'>
+			{props.players.map((player) => (
+				<Player key={player.id} name={player.name} country={player.country} searches={player.searches} />
+			))}
+		</div>
+	);
 }
 
 export default PlayersList;
