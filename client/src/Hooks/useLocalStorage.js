93f7016e6ebffbc464ff6dd8cea73110dd1376e1
Brad <brad@viziworx.com>
2020-04-19 17:38:22 -0400

    refactor code

--- client/src/Hooks/useLocalStorage.js
+++ client/src/Hooks/useLocalStorage.js
@@ -2,10 +2,13 @@ import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
 	const [ storedValue, setStoredValue ] = useState(() => {
+
 		if (window.localStorage.getItem(key)) {
 			return JSON.parse(window.localStorage.getItem(key));
-		}
-		window.localStorage.setItem(key, JSON.stringify(initialValue));
+    }
+
+    window.localStorage.setItem(key, JSON.stringify(initialValue));
+
 		return initialValue;
 	});
 
